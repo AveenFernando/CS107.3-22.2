4b2c4ad68c9c005f977254120277d1bcd0112827
Fernando A N <132265365+AveenFernando@users.noreply.github.com>
2023-08-01 00:15:15 +0530

    Lab 2 Q5 added

---
+++ Lab 2/# Tutorial 2 Q5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace __Tutorial_2_Q5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                Console.Write("Input number : ");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine("The value is even number");
+                }
+                else
+                {
+                    Console.WriteLine("The  is odd number");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
