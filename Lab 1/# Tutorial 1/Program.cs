5f8efdee781758a799ab214bd520c87536f4b6dc
Fernando A N <132265365+AveenFernando@users.noreply.github.com>
2023-07-31 23:48:00 +0530

    Finished Tutorial 1 Q1

---
+++ Lab 1/# Tutorial 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace __Tutorial_1 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            {
+                Console.WriteLine("Input your name :");
+                string name = Console.ReadLine();
+                Console.WriteLine("Input your batch :");
+                double batch = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+                Console.WriteLine("Name : " + name);
+                Console.WriteLine("Batch : " + batch);
+
+                Console.ReadLine();
+            }
+    }
+}
