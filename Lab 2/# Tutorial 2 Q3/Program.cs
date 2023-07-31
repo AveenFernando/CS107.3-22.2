cec8345adaf90691102919ac05064fc3ddf5e416
Fernando A N <132265365+AveenFernando@users.noreply.github.com>
2023-08-01 00:11:24 +0530

    Tutorial 2 Q4 added

---
+++ Lab 2/# Tutorial 2 Q3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace __Tutorial_2_Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Radius:");
+            double rad = double.Parse(Console.ReadLine());
+            double circ;
+            double area;
+            circ = 2 * Math.PI * rad;
+            area = Math.PI * rad * rad;
+
+            Console.WriteLine("Area:" + area);
+            Console.WriteLine("Circumference: " + circ);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab 2/# Tutorial 2 Q4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace __Tutorial_2_Q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a value: ");
+            int val = int.Parse(Console.ReadLine());
+
+            if (val % 2 == 0)
+            {
+                Console.WriteLine("The value is a even number");
+            }
+            else
+            {
+                Console.WriteLine("The value is a odd number");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
